276a5fa96ee6b53e030cc953ae435efeaa28bc1a
esteban-lt <ledezmatenaesteban@gmail.com>
2026-06-23 20:58:01 -0600

    add DTO

---
+++ apps/api/src/organizations/domain/dtos/create-organization-dto.ts
@@ -0,0 +1,20 @@
+export class CreateOrganizationDto {
+
+  private constructor(
+    public readonly name: string,
+    public readonly slug: string,
+  ) {}
+
+  public static create(object: { [key: string]: any }): [string?, CreateOrganizationDto?] {
+    const { name, slug } = object;
+
+    if(!name) return ['name is required'];
+    if(!slug) return ['slug is required'];
+    if(name.length < 3) return ['name must be at least 3 characters long'];
+
+    return [
+      undefined,
+      new CreateOrganizationDto(name, slug)
+    ];
+  }
+}

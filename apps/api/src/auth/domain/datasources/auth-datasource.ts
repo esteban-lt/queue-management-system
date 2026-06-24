ed137c0b12fdd92d5cfc68afedf450dae7ec2df4
esteban-lt <ledezmatenaesteban@gmail.com>
2026-06-23 20:56:31 -0600

    create datasource and repository

---
+++ apps/api/src/auth/domain/datasources/auth-datasource.ts
@@ -0,0 +1,7 @@
+import type { User } from '../entities/user';
+import type { CreateUserData } from '../interfaces/create-user-data';
+
+export abstract class AuthDatasource {
+  public abstract createUser(data: CreateUserData): Promise<User>;
+  public abstract findUserByEmail(email: string): Promise<User | null>;
+}

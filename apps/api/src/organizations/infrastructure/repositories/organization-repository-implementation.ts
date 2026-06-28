43ef371bec1119afbc19ac863fa9d736a8e07e2a
esteban-lt <ledezmatenaesteban@gmail.com>
2026-06-27 21:24:41 -0600

    create repository implementation

--- apps/api/src/organizations/infrastructure/repositories/organization-repository-implementation.ts
+++ apps/api/src/organizations/infrastructure/repositories/organization-repository-implementation.ts
@@ -1,7 +1,8 @@
-import type { OrganizationDatasource } from '../../domain/datasources/organization-datasource';
-import type { CreateOrganizationDto } from '../../domain/dtos/create-organization-dto';
-import type { Organization } from '../../domain/entities/organization';
-import type { OrganizationRepository } from '../../domain/repositories/organization-repository';
+import type { UpdateOrganizationDto } from '@organizations/domain/dtos/update-organization-dto';
+import type { OrganizationDatasource } from '@organizations/domain/datasources/organization-datasource';
+import type { CreateOrganizationDto } from '@organizations/domain/dtos/create-organization-dto';
+import type { Organization } from '@organizations/domain/entities/organization';
+import type { OrganizationRepository } from '@organizations/domain/repositories/organization-repository';
 
 export class OrganizationRepositoryImplementation implements OrganizationRepository {
 
@@ -9,7 +10,19 @@ export class OrganizationRepositoryImplementation implements OrganizationReposit
     private readonly organizationDatasource: OrganizationDatasource,
   ) {}
 
+  public getOrganizations(id: string): Promise<Organization[]> {
+    return this.organizationDatasource.getOrganizations(id);
+  }
+
+  public getOrganizationById(id: string): Promise<Organization> {
+    return this.organizationDatasource.getOrganizationById(id);
+  }
+
   public createOrganization(createOrganizationDto: CreateOrganizationDto): Promise<Organization> {
     return this.organizationDatasource.createOrganization(createOrganizationDto);
+  }
+
+  public updateOrganizationById(id: string, updateOrganizationDto: UpdateOrganizationDto): Promise<Organization> {
+    return this.organizationDatasource.updateOrganizationById(id, updateOrganizationDto);
   }
 }

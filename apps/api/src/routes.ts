fe0216d3ef7e58cecce1e05bea24897fd9427692
esteban-lt <ledezmatenaesteban@gmail.com>
2026-07-04 22:02:24 -0600

    feat: add /api/users route

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
+import { AuthMiddleware } from '@auth/presentation/middlewares/auth-middleware';
 import { AuthRoutes } from '@auth/presentation/routes/auth-routes';
 import { BranchRoutes } from '@branches/presentation/routes/branch-routes';
 import { OrganizationRoutes } from '@organizations/presentation/routes/organization-routes';
-import { AuthMiddleware } from '@auth/presentation/middlewares/auth-middleware';
+import { UserRoutes } from '@users/presentation/routes/user-routes';
 
 export class Routes {
 
@@ -13,6 +14,7 @@ export class Routes {
     router.use('/api/auth', AuthRoutes.routes);
     router.use('/api/organizations', AuthMiddleware.verifyAuth, OrganizationRoutes.routes);
     router.use('/api/branches', AuthMiddleware.verifyAuth, BranchRoutes.routes);
+    router.use('/api/users', AuthMiddleware.verifyAuth, UserRoutes.routes);
 
     return router;
   }

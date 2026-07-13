864440dc6fd1acc8118f3ae4149129fa1c3aaa48
esteban-lt <ledezmatenaesteban@gmail.com>
2026-07-12 22:41:53 -0600

    feat: add '/api/service-categories' endpoint

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthMiddleware } from '@auth/presentation/middlewares/auth-middleware';
 import { AuthRoutes } from '@auth/presentation/routes/auth-routes';
 import { BranchRoutes } from '@branches/presentation/routes/branch-routes';
 import { OrganizationRoutes } from '@organizations/presentation/routes/organization-routes';
+import { ServiceCategoryRoutes } from '@service-categories/presentation/routes/service-category-routes';
 import { UserRoutes } from '@users/presentation/routes/user-routes';
 
 export class Routes {
@@ -12,8 +13,9 @@ export class Routes {
     const router = Router();
 
     router.use('/api/auth', AuthRoutes.routes);
-    router.use('/api/organizations', AuthMiddleware.verifyAuth, OrganizationRoutes.routes);
     router.use('/api/branches', AuthMiddleware.verifyAuth, BranchRoutes.routes);
+    router.use('/api/organizations', AuthMiddleware.verifyAuth, OrganizationRoutes.routes);
+    router.use('/api/service-categories', AuthMiddleware.verifyAuth, ServiceCategoryRoutes.routes);
     router.use('/api/users', AuthMiddleware.verifyAuth, UserRoutes.routes);
 
     return router;

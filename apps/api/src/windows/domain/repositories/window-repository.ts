be027fcb89c78e30b07a457be6b2bf1897058222
esteban-lt <ledezmatenaesteban@gmail.com>
2026-07-19 19:10:55 -0600

    create window repository

---
+++ apps/api/src/windows/domain/repositories/window-repository.ts
@@ -0,0 +1,13 @@
+import type { CreateWindowDto } from '../dtos/create-window-dto';
+import type { UpdateWindowDto } from '../dtos/update-window-dto';
+import type { Window } from '../entities/window';
+
+export abstract class WindowRepository {
+  public abstract findAll(organizationId: string): Promise<Window[]>;
+  public abstract findByBranchId(branchId: string): Promise<Window[]>;
+  public abstract findById(id: string): Promise<Window | null>;
+  public abstract findByServiceCategoryId(serviceCategoryId: string): Promise<Window[]>;
+  public abstract save(dto: CreateWindowDto): Promise<Window>;
+  public abstract updateById(id: string, dto: UpdateWindowDto): Promise<Window>;
+  public abstract toggleById(id: string): Promise<Window>;
+}

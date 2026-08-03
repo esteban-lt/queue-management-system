a89215aae64afc0456b7ad75fe83437e26de8c26
esteban-lt <ledezmatenaesteban@gmail.com>
2026-08-02 21:20:26 -0600

    feat: add '/api/tickets' route

--- apps/api/src/routes.ts
+++ apps/api/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthRoutes } from '@auth/presentation/routes/auth-routes';
 import { BranchRoutes } from '@branches/presentation/routes/branch-routes';
 import { OrganizationRoutes } from '@organizations/presentation/routes/organization-routes';
 import { ServiceCategoryRoutes } from '@service-categories/presentation/routes/service-category-routes';
+import { TicketRoutes } from '@tickets/presentation/routes/ticket-routes';
 import { UserRoutes } from '@users/presentation/routes/user-routes';
 
 export class Routes {
@@ -16,6 +17,7 @@ export class Routes {
     router.use('/api/branches', AuthMiddleware.verifyAuth, BranchRoutes.routes);
     router.use('/api/organizations', AuthMiddleware.verifyAuth, OrganizationRoutes.routes);
     router.use('/api/service-categories', AuthMiddleware.verifyAuth, ServiceCategoryRoutes.routes);
+    router.use('/api/tickets', AuthMiddleware.verifyAuth, TicketRoutes.routes);
     router.use('/api/users', AuthMiddleware.verifyAuth, UserRoutes.routes);
 
     return router;
